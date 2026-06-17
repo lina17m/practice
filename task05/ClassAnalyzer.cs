e88974418f4cc957aea3f35537314a84333b412b
Polina Mosina <linamosina815@gmail.com>
2026-06-18 00:30:46 +0600

    Add task05 and task05tests

---
+++ task05/ClassAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task05
+{
+    public class ClassAnalyzer
+    {
+        private readonly Type _type;
+
+        public ClassAnalyzer(Type type)
+        {
+            _type = type;
+        }
+
+        public IEnumerable<string> GetPublicMethods()
+        {
+            return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
+                        .Select(m => m.Name);
+        }
+
+        public IEnumerable<string> GetMethodParams(string methodname)
+        {
+            var method = _type.GetMethods().FirstOrDefault(m => m.Name == methodname);
+            if (method == null) return Enumerable.Empty<string>();
+
+            var parameters = method.GetParameters()
+                                   .Select(p => $"{p.ParameterType.Name} {p.Name}");
+
+            return parameters.Append($"ReturnType: {method.ReturnType.Name}");
+        }
+
+        public IEnumerable<string> GetAllFields()
+        {
+            return _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+                        .Select(f => f.Name);
+        }
+
+        public IEnumerable<string> GetProperties()
+        {
+            return _type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
+                        .Select(p => p.Name);
+        }
+
+        public bool HasAttribute<T>() where T : Attribute
+        {
+            return _type.GetCustomAttribute<T>() != null;
+        }
+    }
+}
