9374b160b015cfe462e22e052efbd50fa4951be2
Polina Mosina <linamosina815@gmail.com>
2026-06-27 23:34:07 +0600

    Add task11tests and solution

---
+++ task11tests/ClassGeneratorTests.cs
@@ -0,0 +1,18 @@
+﻿using task11;
+using Xunit;
+
+namespace task11tests;
+
+public class ClassGeneratorTests
+{
+    [Fact]
+    public void TestCalculation()
+    {
+        ICalculator calc = ClassGenerator.CreateCalculator();
+
+        Assert.Equal(8, calc.Add(5, 3));
+        Assert.Equal(2, calc.Minus(5, 3));
+        Assert.Equal(15, calc.Mul(5, 3));
+        Assert.Equal(2, calc.Div(6, 3));
+    }
+}
